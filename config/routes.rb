e2692f6d8534374a104fe7172d7093c2fdf40fe1
Itsuki0208 <greenday0208@gmail.com>
2022-03-21 06:00:12 +0000

    制作開始

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,21 @@ Rails.application.routes.draw do
   scope module: :public do
     root 'homes#top'
   end
-  
+
   namespace :public do
     # ↓topページをルートパスにするために設定する。↓
     resources :members, only:[:show, :edit, :update]
   end
-
-  
-
 
   devise_for :admins, {
     sessions: 'admins/sessions',
     passwords: 'admins/passwords',
     registrations: 'admins/registrations'
   }
-  
+
   namespace :admin do
     root 'homes#top'
+    resources :members, only: [:index, :show ]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
